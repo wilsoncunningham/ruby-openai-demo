81e345731f1cde6418661e39daf19a346656d41a
wilsoncunningham <122071360+wilsoncunningham@users.noreply.github.com>
2025-01-24 02:34:56 +0000

    setting up gems and api

---
+++ Gemfile
@@ -0,0 +1,4 @@
+source "https://rubygems.org"
+
+gem "ruby-openai"
+gem "dotenv"
--- chat.rb
+++ chat.rb
@@ -1,1 +1,7 @@
 # Write your solution here!
+require "openai"
+require "dotenv/load"
+
+client = OpenAI::Client.new(access_token: ENV.fetch("OPENAI_API_KEY"))
+
+pp "Howdy!"
